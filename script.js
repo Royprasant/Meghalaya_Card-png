eb2c9f3501e017a87a3494585e0995adf2528196
Prashanta Roy <86010845+Royprasant@users.noreply.github.com>
2025-03-13 16:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+}, 5000); // 5 seconds total delay
